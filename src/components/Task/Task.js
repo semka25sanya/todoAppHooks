22c5bc09c799e624abc5431b5e04e1cc88752438
Alexa Semyonova <semeenowa.alexandra@yandex.ru>
2023-03-01 00:57:29 +0300

    correction

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -2,6 +2,7 @@ import { Component } from 'react'
 import './Task.css'
 import formatDistanceToNow from 'date-fns/formatDistanceToNow'
 import PropTypes from 'prop-types'
+import classNames from 'classnames'
 
 export default class Task extends Component {
     static defaultProps = {
@@ -24,13 +25,17 @@ export default class Task extends Component {
     }
 
     componentDidMount() {
-        setInterval(() => {
+        this.timerID = setInterval(() => {
             this.setState({
                 currentTime: formatDistanceToNow(this.props.data, {
                     includeSeconds: true,
                 }),
             })
         }, 1000)
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID)
     }
 
     newStateEdit = (ev) => {
@@ -43,14 +48,7 @@ export default class Task extends Component {
     }
 
     render() {
-        const { description, onDeleted, onToggleCompleted, completed, onEdit, edit, editTodoSubmit, id } = this.props
-
-        let className = 'todo-list-item'
-
-        if (completed) {
-            className += ' completed'
-        }
-
+        const { description, onDeleted, onToggleCompleted, onEdit, completed, edit, editTodoSubmit, id } = this.props
         return edit ? (
             <li className="editing">
                 <form onSubmit={editTodoSubmit}>
@@ -64,7 +62,7 @@ export default class Task extends Component {
                 </form>
             </li>
         ) : (
-            <li className={className}>
+            <li className={classNames('todo-list-item', { ' completed': completed })}>
                 <div className="view">
                     <input id={id} className="toggle" type="checkbox" onClick={onToggleCompleted} />
                     <label htmlFor={id}>
